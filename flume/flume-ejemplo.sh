4e4668996b14ba334087183172046b6f7208c7de
Neftalí Valdez <neftalivaldez@gmail.com>
2021-07-30 09:42:26 -0700

    flume sobre centos 7

--- flume/flume-ejemplo.sh
+++ flume/flume-ejemplo.sh
@@ -1,26 +1,19 @@
+cat /etc/redhat-release
+echo $JAVA_HOME
+
 ## flume 1
 #Instalamos Flume
 wget https://apache.claz.org/flume/1.9.0/apache-flume-1.9.0-bin.tar.gz
 sudo tar -zxvf apache-flume-1.9.0-bin.tar
 cd apache-flume-1.9.0-bin
-sudo mv * /usr/local/flume
-sudo rm -r apache-flume-1.9.0-bin
-sudo rm apache-flume-1.9.0-bin.tar 
-
-export FLUME_HOME=/usr/local/flume/
+export FLUME_HOME=/home/centos/apache-flume-1.9.0-bin
 
 
 # Instalamos Kakfa
-sudo mkdir kafka
-
 cd kafka
 wget https://downloads.apache.org/kafka/2.8.0/kafka_2.13-2.8.0.tgz
-sudo mv /Users/neftalivaldez/Downloads/kafka_2.13-2.8.0.tar /usr/local/kafka
 tar -xzvf kafka_2.13-2.8.0
-cd kafka
-sudo mv * /usr/local/kafka
-cd ..
-sudo rm -R kafka_2.13-2.8.0
+export KAFKA_HOME=/home/centos/kafka_2.13-2.8.0
 
 # Nueva terminal .- Iniciamos Zookeeper
 cd ${KAFKA_HOME}
@@ -30,13 +23,6 @@ cd ${KAFKA_HOME}
 cd ${KAFKA_HOME}
 ./bin/kafka-server-start.sh config/server.properties
 
-
-pwd
-mkdir flume-data
-
-mkdir data
-vi /flume-data/spool-1
-{"id":0,"firstName":"tomcy","lastName":"john","dob":"1985-10-20"}{"id":1,"firstName":"rahul","lastName":"dev","dob":"1989-08-15"}{"id":2,"firstName":"pankaj","lastName":"misra","dob":"1982-08-10"}{"id":3,"firstName":"devi","lastName":"lal","dob":"1990-05-06"}{"id":4,"firstName":"john","lastName":"doe","dob":"1992-06-25"}
 
 ## Configuración de Flume
 cd $FLUME_HOME
@@ -59,8 +45,7 @@ agent.sinks.kafkaSink.brokerList=localhost:9092
 agent.sinks.kafkaSink.topic=spooled
 agent.sinks.kafkaSink.channel = memoryChannel
 
-## En una nueva terminal
-export FLUME_HOME=/usr/local/flume/
+## En una nueva terminal iniciar Flume
 cd ${FLUME_HOME}
 sudo ./bin/flume-ng agent \
 --conf ${FLUME_HOME}/conf/ \
@@ -68,24 +53,26 @@ sudo ./bin/flume-ng agent \
 -n agent \
 -Dflume.root.logger=INFO,console
 
-# <- Es lo mismo 
-#sudo ./bin/flume-ng agent --conf ${FLUME_HOME}/conf/  -f ${FLUME_HOME}/conf/spool-kafka-flume-conf.properties  -n agent -Dflume.root.logger=INFO,console
 
+## Insertar datos
+cd /home/centos/
+pwd
+mkdir flume-data
 
+cd /home/centos
+vi /flume-data/spool-1
+{"id":0,"firstName":"tomcy","lastName":"john","dob":"1985-10-20"}{"id":1,"firstName":"rahul","lastName":"dev","dob":"1989-08-15"}{"id":2,"firstName":"pankaj","lastName":"misra","dob":"1982-08-10"}{"id":3,"firstName":"devi","lastName":"lal","dob":"1990-05-06"}{"id":4,"firstName":"john","lastName":"doe","dob":"1992-06-25"}
+
+vi flume-data/spool-2
+{"id":10,"firstName":"tomcy","lastName":"john","dob":"1985-10-20"}{"id":11,"firstName":"rahul","lastName":"dev","dob":"1989-08-15"}{"id":12,"firstName":"pankaj","lastName":"misra","dob":"1982-08-10"}{"id":13,"firstName":"devi","lastName":"lal","dob":"1990-05-06"}{"id":14,"firstName":"john","lastName":"doe","dob":"1992-06-25"}
+
+vi flume-data/spool-3
+{"id":20,"firstName":"tomcy","lastName":"john","dob":"1985-10-20"}{"id":21,"firstName":"rahul","lastName":"dev","dob":"1989-08-15"}{"id":22,"firstName":"pankaj","lastName":"misra","dob":"1982-08-10"}{"id":23,"firstName":"devi","lastName":"lal","dob":"1990-05-06"}{"id":24,"firstName":"john","lastName":"doe","dob":"1992-06-25"}
+
+## Para ver los datos insertados
 ## En una nueva terminal
 cat /tmp/kafka-logs/spooled-0/00000000000000000000.log
 
 ## En una nueva terminal - termina kafka
 cd /usr/local/kafka
 sudo ./bin/kafka-console-consumer.sh consumer.properties --topic spooled -bootstrap-server localhost:9092 --from-beginning
-
-
-vi flume-data/spool-2
-{"id":10,"firstName":"tomcy","lastName":"john","dob":"1985-10-20"}{"id":11,"firstName":"rahul","lastName":"dev","dob":"1989-08-15"}{"id":12,"firstName":"pankaj","lastName":"misra","dob":"1982-08-10"}{"id":13,"firstName":"devi","lastName":"lal","dob":"1990-05-06"}{"id":14,"firstName":"john","lastName":"doe","dob":"1992-06-25"}
-
-
-vi flume-data/spool-3
-{"id":20,"firstName":"tomcy","lastName":"john","dob":"1985-10-20"}{"id":21,"firstName":"rahul","lastName":"dev","dob":"1989-08-15"}{"id":22,"firstName":"pankaj","lastName":"misra","dob":"1982-08-10"}{"id":23,"firstName":"devi","lastName":"lal","dob":"1990-05-06"}{"id":24,"firstName":"john","lastName":"doe","dob":"1992-06-25"}
-
-
-
